c280d976c8747b9ddcdb38c5a2ef216c9d26b6b1
Sai809701 <saipawar.2503@gmail.com>
2026-05-04 18:15:49 +0530

    Update layout metadata with new icon configurations

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,11 @@ export const metadata = {
   title: "Portfolio of Sai Pawar - Software Developer",
   description:
     "This is the portfolio of Sai Pawar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: "/profile.png",
+    shortcut: "/profile.png",
+    apple: "/profile.png",
+  },
 };
 
 export default function RootLayout({ children }) {
